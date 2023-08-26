a22e3465b73134bdff32f65ab4335f4cddf4c19a
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-08-26 17:12:29 +0200

    added icon

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect} from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 //import { Icon } from "leaflet";
 
 const Map = ({lat, lng}) => {
 const [position, setPosition] = useState(null);
 
+const icon = new Icon({
+    iconUrl: "https://as1.ftcdn.net/v2/jpg/05/24/16/84/1000_F_524168432_SwASPZ9erHRRBUR4RbI7vDU8dLwZQwvz.webp",
+    iconSize: [50, 50]
+  });
+
 useEffect(() => {
 setPosition([lat, lng])
 }, [lat, lng]);
 
-//console.log(position)
+
+
+
 return (
 <>
 {lat !== 0 && lng !== 0 ? (
@@ -19,7 +27,7 @@ return (
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={position}>
+    <Marker position={position}  icon={icon}>
     <Popup>
         You are here!
     </Popup>
